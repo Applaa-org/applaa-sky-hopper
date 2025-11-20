2524f6f9f62ae537f8ddcddef2331748a581ba6e
[applaa] <git@applaa.sh>
2025-11-20 21:40:36 +0530

    [applaa] wrote 5 file(s)

--- godot-project/scripts/Main.gd
+++ godot-project/scripts/Main.gd
@@ -5,49 +5,70 @@ extends Node
 @onready var player = $Player
 @onready var obstacle_timer = $ObstacleTimer
 @onready var hud = $HUD
-@onready var ground = $Ground
 @onready var parallax_background = $ParallaxBackground
+@onready var ground1 = $Ground1
+@onready var ground2 = $Ground2
+
+const SCROLL_SPEED = 150.0
 
 var score = 0
-var is_playing = false
+var ground_width = 0
 
 func _ready():
 	hud.update_score(score)
 	hud.show_start_screen()
 	player.hit.connect(_on_player_hit)
+	set_physics_process(false)
+	# Calculate ground width for seamless looping
+	ground_width = ground1.get_node("ColorRect").size.x
 
 func new_game():
 	score = 0
-	is_playing = true
 	player.start()
 	hud.update_score(score)
 	hud.show_gameplay_ui()
 	obstacle_timer.start()
+	set_physics_process(true)
+	
+	# Reset ground positions
+	ground1.position.x = 0
+	ground2.position.x = ground_width
 	
 	# Clear any existing obstacles
 	for n in get_tree().get_nodes_in_group("obstacles"):
 		n.queue_free()
 
+func _physics_process(delta):
+	# Scroll parallax background
+	parallax_background.scroll_offset.x += SCROLL_SPEED * 0.5 * delta
+	
+	# Scroll ground
+	ground1.position.x -= SCROLL_SPEED * delta
+	ground2.position.x -= SCROLL_SPEED * delta
+	
+	# Loop ground
+	if ground1.position.x < -ground_width:
+		ground1.position.x += 2 * ground_width
+	if ground2.position.x < -ground_width:
+		ground2.position.x += 2 * ground_width
+
 func _on_obstacle_timer_timeout():
 	var obstacle = obstacle_scene.instantiate()
 	obstacle.position.x = get_viewport_rect().size.x + 100
-	# Randomize vertical position of the gap
 	obstacle.position.y = randf_range(200, 550)
+	obstacle.scored.connect(_on_obstacle_scored)
 	add_child(obstacle)
 	obstacle.add_to_group("obstacles")
 
 func _on_player_hit():
-	is_playing = false
+	set_physics_process(false)
 	obstacle_timer.stop()
-	ground.stop()
-	parallax_background.stop()
 	
 	for n in get_tree().get_nodes_in_group("obstacles"):
-		n.set_process(false)
+		n.stop()
 		
 	hud.show_game_over_screen(score)
 
-func _on_score_area_body_entered(body):
-	if body is Player:
-		score += 1
-		hud.update_score(score)
+func _on_obstacle_scored():
+	score += 1
+	hud.update_score(score)
---
+++ godot-project/scripts/Obstacle.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+signal scored
+
+const MOVE_SPEED = 150.0
+
+func _process(delta):
+	position.x -= MOVE_SPEED * delta
+
+func _on_score_area_body_entered(body):
+	if body is Player:
+		emit_signal("scored")
+		# Disable the area so it doesn't score again
+		$ScoreArea.monitoring = false
+
+func stop():
+	set_process(false)
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
